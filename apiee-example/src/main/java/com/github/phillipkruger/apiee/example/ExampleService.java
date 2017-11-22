601cee37a126692770134f63bc2debd6aaa3d5a8
Phillip Kruger <phillip.kruger@gmail.com>
2017-11-22 20:05:16 +0200

    Added header example

--- apiee-example/src/main/java/com/github/phillipkruger/apiee/example/ExampleService.java
+++ apiee-example/src/main/java/com/github/phillipkruger/apiee/example/ExampleService.java
@@ -1,10 +1,13 @@
 package com.github.phillipkruger.apiee.example;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import java.util.logging.Level;
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -40,6 +43,9 @@ public class ExampleService {
     @Context
     private UriInfo uriInfo;  
     
+    @Context
+    private HttpServletRequest request;
+    
     @POST
     @ApiOperation(value = "Post some example content", notes = "This will post some json to the server")
     public Response postExample(JsonObject jsonObject) {
@@ -61,4 +67,16 @@ public class ExampleService {
         log.log(Level.INFO, "DELETE");
     }
     
+    
+    @GET
+    @Path("/header")
+    @ApiOperation(value = "Pass header value in", notes = "Some header info")
+    @ApiImplicitParams({ @ApiImplicitParam(name = "authorization",
+                                         value = "Some info.",
+                                         dataType = "string",
+                                         paramType = "header") })
+    public String headerExample(){
+        String header = request.getHeader("authorization");
+        return "You have passes [" + header + "] in the authorization header";
+    }
 }
